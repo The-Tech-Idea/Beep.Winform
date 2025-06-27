72871fd1080a40d7093d13d7054bbdae8955f033
fahad@thetechidea.com <fahad@thetechidea.com>
2025-06-27 20:28:58 +0300

    updated python methods and nodes

---
+++ TheTechIdea.Beep.Winform.Default.Views/MainFrm_SideBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TheTechIdea.Beep.Container.Services;
+using TheTechIdea.Beep.Editor;
+using TheTechIdea.Beep.Vis.Modules;
+using TheTechIdea.Beep.Winform.Controls;
+using TheTechIdea.Beep.Winform.Controls.ITrees.BeepTreeView;
+using TheTechIdea.Beep.Winform.Controls.MenuBar;
+using TheTechIdea.Beep.Winform.Default.Views.Template;
+
+namespace TheTechIdea.Beep.Winform.Default.Views
+{
+    public partial class MainFrm_SideBar : TemplateForm
+    {
+      
+      
+
+        public MainFrm_SideBar()
+        {
+            InitializeComponent();
+
+        }
+        public MainFrm_SideBar(IBeepService service) : base(service)
+        {
+            InitializeComponent();
+            beepService.vis.Container = beepDisplayContainer1;
+            beepService.vis.Container.ContainerType = ContainerTypeEnum.TabbedPanel;
+
+
+     
+
+        }
+    }
+}
---
+++ TheTechIdea.Beep.Winform.Default.Views/MainFrm_Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TheTechIdea.Beep.Container.Services;
+using TheTechIdea.Beep.Vis.Modules;
+using TheTechIdea.Beep.Winform.Controls;
+using TheTechIdea.Beep.Winform.Default.Views.Template;
+
+namespace TheTechIdea.Beep.Winform.Default.Views
+{
+    public partial class MainFrm_Tree :  TemplateForm
+    {
+     
+
+        public MainFrm_Tree()
+        {
+            InitializeComponent();
+
+        }
+        public MainFrm_Tree(IBeepService service) : base(service)
+        {
+            InitializeComponent();
+            beepService.vis.Container = beepDisplayContainer1;
+            beepService.vis.Container.ContainerType = ContainerTypeEnum.TabbedPanel;
+
+            beepAppTree1.init(beepService);
+            beepAppTree1.CreateRootTree();
+         
+
+            beepMenuAppBar1.beepServices = beepService;
+            beepMenuAppBar1.CreateMenuItems();
+
+
+        }
+    }
+}

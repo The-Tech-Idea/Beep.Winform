21ef39a3aa23403f9aa037117c5394d0260cffb6
fahad@thetechidea.com <fahad@thetechidea.com>
2026-01-28 07:42:34 +0300

    update

--- TheTechIdea.Beep.Winform.Default.Views/Template/TemplateUserControl.cs
+++ TheTechIdea.Beep.Winform.Default.Views/Template/TemplateUserControl.cs
@@ -189,14 +189,22 @@ namespace TheTechIdea.Beep.Winform.Default.Views.Template
             BeepThemesManager.FormStyleChanged -= BeepThemesManager_FormStyleChanged;
             _themeEventsRegistered = false;
         }
-
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
             if (disposing)
             {
                 UnregisterThemeEvents();
             }
             base.Dispose(disposing);
         }
+      
     }
 }

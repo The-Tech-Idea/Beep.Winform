91ec4980bbc93ade00a65023f19b334bd0d66d68
fahad aldhubaib <f_ald@DEVSTATION>
2026-06-01 09:35:32 +0300

    updated and removed Views and Integrated to sepearte project

--- TheTechIdea.Beep.Winform.Controls.Integrated/Forms/Models/BeepFormsQueryShelfCaptionMode.cs
+++
@@ -1,9 +0,0 @@
-namespace TheTechIdea.Beep.Winform.Controls.Integrated.Forms.Models
-{
-    public enum BeepFormsQueryShelfCaptionMode
-    {
-        TitleOnly = 0,
-        TargetOnly = 1,
-        TargetPlusMode = 2
-    }
-}
--- TheTechIdea.Beep.Winfrom.Controls.472/Modules/ThemeTypes/FlatDesignTheme/FlatDesignTheme.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace TheTechIdea.Beep.Vis.Modules
-{
-    public partial class FlatDesignTheme  : IBeepTheme
-    {
-        public FlatDesignTheme() { }
-    }
-}
